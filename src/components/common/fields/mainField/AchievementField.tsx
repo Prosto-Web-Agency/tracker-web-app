cf515ececfe5978be39b0fa31a6bc2820f618933
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-12-10 05:39:15 +0300

    fix build for chart

--- src/components/common/fields/mainField/AchievementField.tsx
+++ src/components/common/fields/mainField/AchievementField.tsx
@@ -2,7 +2,7 @@ import SwiperComponent from "@/components/common/swiper";
 
 export default function AchievmentField() {
     return (
-        <div className="w-[282px] lg:w-full h-[300px] bg-white overflow-hidden bg-orange-class">
+        <div className="w-[282px] relative lg:w-full h-[300px] bg-white overflow-hidden bg-orange-class">
             <h3 className="text-heading-s mt-3 ml-4 text-white s_lg:text-heading-ss-bold pb-2">
                 Ранги
             </h3>
--- src/components/common/swiper/index.tsx
+++ src/components/common/swiper/index.tsx
@@ -1,41 +1,63 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import AchievmentCard from "@/components/common/cards/mainPageCards/AchievmentCard";
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function SwiperComponent<T>({ swiperCards, cardType }: { swiperCards: T[], cardType: 'achievement' }) {
+    const swiper = useSwiper();
+
     return (
-        <Swiper
-            spaceBetween={20}
-            slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            loop={true}
-        >
-            {
-                swiperCards.map((card, index) => (
-                    <SwiperSlide
-                        style={{
-                            width: '280px',
-                            display: 'flex',
-                            justifyContent: 'center'
-                        }}
-                        key={index}
-                    >
-                        {
-                            cardType === 'achievement' ?
-                                (
-                                    // @ts-ignore
-                                    <AchievmentCard {...card} />
-                                ) : (
-                                    <>Slide 1</>
-                                )
-                        }
-                    </SwiperSlide>
-                ))
-            }
-        </Swiper>
+        <>
+            <button
+                className="z-[900] bg-white rounded-5 top-[calc(50%-20px)] right-[2%] absolute w-[40px] h-[40px]"
+                onClick={() => swiper.slideNext()}
+            />
+
+            <button
+                className="z-[900] bg-white rounded-5 top-[calc(50%-20px)] left-[2%] absolute w-[40px] h-[40px]"
+                onClick={() => swiper.slidePrev()}
+            />
+
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={20}
+                slidesPerView={1}
+                loop={true}
+            >
+                {
+                    swiperCards.map((card, index) => (
+                        <SwiperSlide
+                            style={{
+                                width: '280px',
+                                display: 'flex',
+                                justifyContent: 'center'
+                            }}
+                            key={index}
+                        >
+                            {
+                                cardType === 'achievement' ?
+                                    (
+                                        // @ts-ignore
+                                        <AchievmentCard {...card} />
+                                    ) : (
+                                        <>Slide 1</>
+                                    )
+                            }
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </>
     );
 };
