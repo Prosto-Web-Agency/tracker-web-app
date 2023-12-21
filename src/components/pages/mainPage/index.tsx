816456824a393eeb85818630b0b7852ce7673a77
Неволин Олег <nevolin.oleg@wildberries.ru>
2023-12-21 05:26:51 +0300

    update reports page

--- src/components/pages/mainPage/index.tsx
+++ src/components/pages/mainPage/index.tsx
@@ -10,12 +10,12 @@ import { getListOfUsersInsights, getListOfTopUsers } from "@/store/thunks/traker
 import { useDispatch, useSelector } from "react-redux";
 import SearchUsers from "@/components/common/fields/mainField/SearchUsers";
 import UserSubscriptions from "@/components/common/userSubscription";
+import {useAppSelector} from "@/hooks/store";
 
 export default function MainPage() {
     const dispatch = useDispatch();
 
-    //@ts-ignore
-    const { listOfTopUsers, listOfUserInsights } = useSelector(state => state.mainPage)
+    const { listOfTopUsers, listOfUserInsights } = useAppSelector(state => state.mainPage)
 
     useEffect(() => {
         // @ts-ignore
@@ -27,7 +27,7 @@ export default function MainPage() {
     return (
         <section className="w-full">
             <div className="min-h-[calc(100vh-90px)] relative w-full bg-bg-gray rounded-9 p-10 max-w-[1400px] mx-auto s_lg:rounded-t-[0px] s_lg:pt-0 s_lg:p-6 pb-3 s_lg:pb-12">
-                <div className="max-h-[900px] big_screen_h:h-[calc(65vh-130px)] flex gap-6 s_lg:flex-col-reverse s_lg:h-auto">
+                <div className="max-h-[900px] flex gap-6 s_lg:flex-col-reverse s_lg:h-auto">
                     <div className="flex flex-col gap-6">
                         <SearchUsers />
                         <ListOfInsightCards listOfUserInsights={listOfUserInsights} />
--- src/components/pages/reports/index.tsx
+++ src/components/pages/reports/index.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 import BasePrimaryCard from "@/components/common/cards/BasePrimaryCard";
-import React from "react";
+import React, {useEffect} from "react";
 import SearchUsers from "@/components/common/fields/mainField/SearchUsers";
+import {getUserSubscriptionsReportsData} from "@/store/thunks/userThunk";
+import {useAppDispatch, useAppSelector} from "@/hooks/store";
+import {TUserSubscriptionsReportArray} from "@/models/userData";
+import SubscriptionCard from "@/components/common/userSubscription/subscriptionCard";
 
-export default function ReportsPage(){
-    return(
+export default function ReportsPage() {
+    const dispatch = useAppDispatch();
+    const {userSubscriptionsReports}: {
+        userSubscriptionsReports: TUserSubscriptionsReportArray
+    } = useAppSelector(state => state.user)
+
+
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(getUserSubscriptionsReportsData());
+    }, [dispatch]);
+
+    return (
         <section className='w-full bg-bg-gray h-full p-10 rounded-t-9 flex gap-6 s_lg:rounded-t-[0px] s_lg:flex-col'>
             <div className='flex flex-col gap-6 w-full flex-[33%] max-w-[380px] min-w-[290px] s_lg:max-w-full'>
                 <SearchUsers/>
@@ -15,9 +30,35 @@ export default function ReportsPage(){
             </div>
             <div className='flex-[67%]'>
                 <BasePrimaryCard>
-                    <h3 className="text-heading-ss-bold pb-2">
-                        Отчеты
-                    </h3>
+                    <div className='p-7 h-full overflow-hidden'>
+                        <h3 className="text-heading-ss-bold pb-2">
+                            Отчеты
+                        </h3>
+                        {
+                            userSubscriptionsReports.length &&
+                            <div className="flex flex-col overflow-y-auto overflow-x-hidden w-full h-full">
+                                {
+                                    userSubscriptionsReports.map(({
+                                                                      report_text,
+                                                                      first_name,
+                                                                      image_url,
+                                                                      login,
+                                                                      is_anon
+                                                                  }, index) => (
+                                        <SubscriptionCard
+                                            key={first_name + index}
+                                            report_text={report_text}
+                                            first_name={first_name}
+                                            image_url={image_url}
+                                            is_anon={is_anon}
+                                            login={login}
+                                        />
+                                    ))
+                                }
+                            </div>
+
+                        }
+                    </div>
                 </BasePrimaryCard>
             </div>
         </section>
