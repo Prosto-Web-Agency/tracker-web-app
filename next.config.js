f59ab871a99d9df2d6629c87a6450d1eab48c648
Неволин Олег <nevolin.oleg@wildberries.ru>
2023-12-22 11:32:09 +0300

    ignoreDuringBuilds: true,

--- next.config.js
+++ next.config.js
@@ -13,4 +13,7 @@ module.exports = withImages({
     images: {
         domains: ['storage.yandexcloud.net', 'cdn4.cdn-telegram.org', 'cdn5.cdn-telegram.org'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 });
