7f70dcb3ab0d9cf9ebda6be3a1b001ffc3b57fc1
Lev <levlevlavrov@gmail.com>
2023-12-25 17:09:12 +0300

    userSearch

--- src/components/common/fields/mainField/SearchUsers.tsx
+++ src/components/common/fields/mainField/SearchUsers.tsx
@@ -77,7 +77,7 @@ export default function SearchUsers() {
       />
 
       {text === '' ? null : (
-        <div className="flex flex-col gap-2 bg-white rounded-4 shadow-primary mt-4 p-6 min-h-[100px] max-h-[240px] w-full absolute">
+        <div className="flex flex-col gap-2 bg-white rounded-4 z-[100] shadow-primary mt-4 p-6 min-h-[100px] max-h-[240px] w-full absolute">
           {searchUsers.map(
             (
               {
